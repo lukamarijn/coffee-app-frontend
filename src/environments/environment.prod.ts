238ffe65ec5c957c85e800399c9879021b74c289
lukamarijn <lukabrinkmannn@gmail.com>
2017-12-12 23:11:38 +0100

    fixessss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // heroku server url
-  serverUrl: 'https://coffee-app-frontend.herokuapp.com/',
+  serverUrl: 'https://coffee-app-backend.herokuapp.com/api/v1',
   production: true
 };
